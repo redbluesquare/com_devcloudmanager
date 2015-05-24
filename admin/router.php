cd8d1180e3f979974477e4e534de062d75fb5966
Darryl <Darryl@Corban>
2015-05-24 22:21:30 +0100

    update folders

---
+++ admin/router.php
@@ -0,0 +1,13 @@
+<?php // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' ); 
+
+function DevcloudmanagerBuildRoute(&$query)
+{
+	$segments = array();
+	return $segments;
+}
+
+function DevcloudmanagerParseRoute($segments) 
+{
+
+}
